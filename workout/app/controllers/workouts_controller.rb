bae8b2f817b5488c32b628b09a01584087bd68be
Raj Bennin <rajisteb@gmail.com>
2017-10-15 12:41:17 -0400

    add create functionality and render on show page

--- workout/app/controllers/workouts_controller.rb
+++ workout/app/controllers/workouts_controller.rb
@@ -1,7 +1,45 @@
 class WorkoutsController < ApplicationController
+    before_action :find_workout, only: [:show, :edit, :update, :destroy]
+
 
     def index
         @workout = Exercise.all
     end
+
+    def show
+    end
+
+    def new
+        @workout = Exercise.new
+    end
+
+    def create
+        workout = Exercise.new(post_workout)
+        if workout.save
+            redirect_to workout_path(workout)
+        else
+            redirect_to new_workout_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def post_workout
+        params.require(:exercise).permit(:name, :category, :reps, :example, :progression)
+    end
+
+    def find_workout
+        @workout = Exercise.find(params[:id])
+    end
+
 
 end
